c0b508842a1f30a1171bd70a968c4229fc65cb34
Natalka <nazarjazz@wp.pl>
2017-08-05 16:05:31 +0200

    ChooseplayerActivity build

--- app/src/main/java/pl/nataliana/detectivehardestcase/ChooseplayerActivity.java
+++ app/src/main/java/pl/nataliana/detectivehardestcase/ChooseplayerActivity.java
@@ -1,5 +1,6 @@
 package pl.nataliana.detectivehardestcase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,11 +22,17 @@ public class ChooseplayerActivity extends AppCompatActivity {
     @BindView(R.id.man_img)
     ImageView man_img;
     @BindView(R.id.name)
-    EditText name;
+    EditText nameEdit;
     @BindView(R.id.user_configured)
     Button user_configured;
     //This variable will define which image should we use to present player's face
     public int playerFace;
+    //This variable will count the score in the game
+    public int score;
+    //This variable will keep player's name
+    public String name;
+    //This variable will keep player's school
+    public String school;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,5 +63,42 @@ public class ChooseplayerActivity extends AppCompatActivity {
                 playerFace = 10;
             }
         });
+    }
+
+    //this method is called when the user chooses 1st school
+    public void clickSchool1(View view) {
+        school = "Harvard";
+        score = 0;
+        score =+ 20;
+
+    }
+
+    //this method is called when the user chooses 2nd school
+    public void clickSchool2(View view) {
+        school = "Arkanas State University";
+        score = 0;
+        score =+ 50;
+
+    }
+
+    //this method is called when the user chooses 3rd school
+    public void clickSchool3(View view) {
+        school = "School of Life";
+        score = 0;
+        score =+ 10;
+
+    }
+
+    //this method is called when the user clicks "done" button
+    public void startStory(View view) {
+        name = nameEdit.getText().toString().trim();
+        Intent i = new Intent(this, pl.nataliana.detectivehardestcase.StoryActivity.class);
+        Bundle extras = new Bundle();
+        extras.putString("name", name);
+        extras.putString("school", school);
+        extras.putInt("facePic", playerFace);
+        extras.putInt("score", score);
+        finish();
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/pl/nataliana/detectivehardestcase/StoryActivity.java
@@ -0,0 +1,8 @@
+package pl.nataliana.detectivehardestcase;
+
+/**
+ * Created by Natalka on 2017-08-05.
+ */
+
+public class StoryActivity {
+}
