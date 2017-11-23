2eec78ade79800651921c79e834df9c3239c4f6b
Natalia Nazaruk <nazarjazz@wp.pl>
2017-11-23 16:14:39 +0100

    MurderSceneActivity built, strings added, logic added

--- app/src/main/java/pl/nataliana/detectivehardestcase/MurderSceneActivity.java
+++ app/src/main/java/pl/nataliana/detectivehardestcase/MurderSceneActivity.java
@@ -1,10 +1,13 @@
 package pl.nataliana.detectivehardestcase;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,17 +16,17 @@ public class MurderSceneActivity extends AppCompatActivity {
 
     @BindView(R.id.goal_tv)
     TextView goal_tv;
-    @BindView(R.id.head_tv)
-    TextView head_tv;
-    @BindView(R.id.statue_tv)
-    TextView statue_tv;
-    @BindView(R.id.book_tv)
-    TextView book_tv;
-    @BindView(R.id.glasses_tv)
-    TextView glasses_tv;
-    @BindView(R.id.body_tv)
-    TextView body_tv;
-
+    @BindView(R.id.head_btn)
+    Button head_btn;
+    @BindView(R.id.statue_btn)
+    Button statue_btn;
+    @BindView(R.id.book_btn)
+    Button book_btn;
+    @BindView(R.id.glasses_btn)
+    Button glasses_btn;
+    @BindView(R.id.body_btn)
+    Button body_btn;
+    private int valueOfItemsFound = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +34,50 @@ public class MurderSceneActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //Bind the view with a layout
-        setContentView(R.layout.activity_chooseplayer);
+        setContentView(R.layout.activity_murder_scene);
         ButterKnife.bind(this);
 
-        //This decides about visibility of views
-        head_tv.setVisibility(View.INVISIBLE);
+        goal_tv.setText(getString(R.string.found_items, valueOfItemsFound));
+
+        head_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valueOfItemsFound++;
+                goal_tv.setText(getString(R.string.found_items, valueOfItemsFound));
+                Toast.makeText(MurderSceneActivity.this, R.string.new_evidence, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        statue_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valueOfItemsFound++;
+                goal_tv.setText(getString(R.string.found_items, valueOfItemsFound));
+                Toast.makeText(MurderSceneActivity.this, R.string.new_evidence, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        book_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MurderSceneActivity.this, R.string.books, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        glasses_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MurderSceneActivity.this, R.string.glasses, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        body_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MurderSceneActivity.this, R.string.body, Toast.LENGTH_LONG).show();
+            }
+        });
+
 
     }
 
--- app/src/main/java/pl/nataliana/detectivehardestcase/ScenesListActivity.java
+++ app/src/main/java/pl/nataliana/detectivehardestcase/ScenesListActivity.java
@@ -1,7 +1,9 @@
 package pl.nataliana.detectivehardestcase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
@@ -27,5 +29,35 @@ public class ScenesListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scenes_list);
         ButterKnife.bind(this);
 
+        scene_1_title_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent c = new Intent(ScenesListActivity.this, MurderSceneActivity.class);
+                c.putExtras(getIntent().getExtras());
+                startActivity(c);
+            }
+        });
+
+        //TODO open proper activity from here
+        scene_2_title_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent c = new Intent(ScenesListActivity.this, MurderSceneActivity.class);
+                c.putExtras(getIntent().getExtras());
+                startActivity(c);
+            }
+        });
+
+        //TODO open proper activity from here
+        scene_3_title_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent c = new Intent(ScenesListActivity.this, MurderSceneActivity.class);
+                c.putExtras(getIntent().getExtras());
+                startActivity(c);
+            }
+        });
     }
+
+
 }
